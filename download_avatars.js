03e52ac2a8b768a1989b34d83b2404aa3012ab49
karen1au <kagemikaka@hotmail.com>
2019-03-11 21:34:30 +0000

    added download image function

--- download_avatars.js
+++ download_avatars.js
@@ -1,5 +1,6 @@
 var request = require('request');
-var secrets = require('./secrets')
+var secrets = require('./secrets');
+var fs = require('fs');
 
 function getRepoContributors(repoOwner, repoName, cb) {
 
@@ -7,18 +8,43 @@ function getRepoContributors(repoOwner, repoName, cb) {
     url: "https://api.github.com/repos/" + repoOwner + "/" + repoName + "/contributors",
     headers: {
     'User-Agent': 'request',
-    'Authorization': secrets.GITHUB_TOKEN
+    'Authorization': 'token ' + secrets.GITHUB_TOKEN
     }
   };
 
   request(options, function(err, res, body) {
-    cb(err, body);
+    // if (!err && res.statusCode == 200){
+    var data = JSON.parse(body);
+
+    cb(err, data);
   // ...
-  });
+
+});
 }
 
-console.log('Welcome to the GitHub Avatar Downloader!');
-getRepoContributors("jquery", "jquery", function(err, result) {
-  console.log("Errors:", err);
-  console.log("Result:", result);
-})
+
+function downloadImageByURL(url, filePath) {
+  request.get(url)
+         .on('error', function (error){
+            throw err;
+         })
+         .on('response', function (response){
+          console.log('Downloading image...');
+        })
+         .pipe(fs.createWriteStream(filePath))
+         .on('finish', function (){
+          console.log('Download completed.')
+         });
+           // ...
+}
+
+downloadImageByURL('https://avatars3.githubusercontent.com/u/1199584?v=4', "test.jpg")
+
+// console.log('Welcome to the GitHub Avatar Downloader!');
+// getRepoContributors("jquery", "jquery", function(err, result) {
+//   for (let i = 0; i < result.length; i++){
+//    // var avatar = result[i][avatar_url];
+//   console.log("Errors:", err);
+//   console.log("Result:", result[i]["avatar_url"]);
+// }
+// });
