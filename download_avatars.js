65fd429adaef31acc5622c8a49834fe8d0821a44
karen1au <kagemikaka@hotmail.com>
2019-03-11 20:42:46 +0000

    added secret token

--- download_avatars.js
+++ download_avatars.js
@@ -1,12 +1,24 @@
 var request = require('request');
+var secrets = require('./secrets')
 
 function getRepoContributors(repoOwner, repoName, cb) {
 
+  var options = {
+    url: "https://api.github.com/repos/" + repoOwner + "/" + repoName + "/contributors",
+    headers: {
+    'User-Agent': 'request',
+    'Authorization': secrets.GITHUB_TOKEN
+    }
+  };
 
+  request(options, function(err, res, body) {
+    cb(err, body);
+  // ...
+  });
 }
 
 console.log('Welcome to the GitHub Avatar Downloader!');
 getRepoContributors("jquery", "jquery", function(err, result) {
   console.log("Errors:", err);
   console.log("Result:", result);
-});
+})
